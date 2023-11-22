8422858be344c0fd511bf427fde47dbe771778e4
DominiqueHernandezPacheco <148931049+DominiqueHernandezPacheco@users.noreply.github.com>
2023-11-21 19:39:01 -0600

    Add files via upload

---
+++ Coche.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.vehiculos;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Coche extends Vehiculos {
+    
+    private int numeroPuertas;
+
+    public Coche(String marca, String modelo, int numeroPuertas) {
+        super(marca, modelo);
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    
+    public void abrirPuertas() {
+        System.out.println("Abriendo las puertas del coche.");
+    }
+}
+
+
